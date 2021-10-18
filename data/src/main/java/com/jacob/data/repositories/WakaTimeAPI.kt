411302b1d4818c1a0889b79205e10f4986f8ebfd
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-10-18 11:32:21 +0530

    Added test endpoint with dtos.

--- data/src/main/java/com/jacob/data/repositories/WakaTimeAPI.kt
+++ data/src/main/java/com/jacob/data/repositories/WakaTimeAPI.kt
@@ -1,9 +1,11 @@
 package com.jacob.data.repositories
 
+import com.jacob.data.dtos.AllTimeDataDTO
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Header
 
 interface WakaTimeAPI {
-    @GET("")
-    suspend fun login(): Response<Void>
+    @GET("/api/v1/users/current/all_time_since_today")
+    suspend fun getData(@Header("Authorization") token: String): Response<AllTimeDataDTO>
 }
