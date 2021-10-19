7f2169153a75495389624199a19556592181d8f6
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-10-19 14:15:39 +0530

    Added HomePage to nav_graph.

---
+++ app/src/main/java/com/jacob/wakatimeapp/home/HomePage.kt
@@ -0,0 +1,27 @@
+package com.jacob.wakatimeapp.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.jacob.wakatimeapp.theme.WakaTimeAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomePage : Fragment() {
+    private val viewModel by viewModels<HomePageViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ) = ComposeView(requireContext()).apply {
+        setContent {
+            WakaTimeAppTheme {
+            }
+        }
+    }
+}
+
