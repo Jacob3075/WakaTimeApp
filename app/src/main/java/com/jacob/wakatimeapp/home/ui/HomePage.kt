72f3a819f2cb231a4af65457f834eecec82ae20a
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-11-03 22:34:40 +0530

    Added UI for OtherStats section in HomePage.

--- app/src/main/java/com/jacob/wakatimeapp/home/ui/HomePage.kt
+++ app/src/main/java/com/jacob/wakatimeapp/home/ui/HomePage.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -18,6 +20,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import coil.compose.rememberImagePainter
@@ -25,7 +28,9 @@ import coil.transform.CircleCropTransformation
 import com.jacob.wakatimeapp.R
 import com.jacob.wakatimeapp.common.models.Time
 import com.jacob.wakatimeapp.common.models.UserDetails
+import com.jacob.wakatimeapp.common.ui.OtherStatsCard
 import com.jacob.wakatimeapp.common.ui.TimeSpentCard
+import com.jacob.wakatimeapp.common.ui.theme.Colors
 import com.jacob.wakatimeapp.common.ui.theme.Gradients
 import com.jacob.wakatimeapp.common.ui.theme.Typography
 import com.jacob.wakatimeapp.common.ui.theme.WakaTimeAppTheme
@@ -57,22 +62,26 @@ class HomePage : Fragment() {
 @Composable
 private fun HomePageContent(viewModel: HomePageViewModel) =
     Scaffold(modifier = Modifier.fillMaxSize()) {
+        val scrollState = rememberScrollState()
         Column(
             modifier = Modifier
-                .padding(all = 20.dp)
-                .padding(top = 24.dp)
+                .padding(horizontal = 20.dp)
+                .padding(top = 30.dp)
+                .verticalScroll(scrollState)
         ) {
             val userDetailsState by viewModel.userDetails.collectAsState(initial = null)
             val weeklyStatsFlow by viewModel.last7DaysStats.collectAsState()
 
             UserDetailsSection(userDetailsState)
-            Spacer(modifier = Modifier.height(22.dp))
+            Spacer(modifier = Modifier.height(25.dp))
             TimeSpentSection(weeklyStatsFlow?.todaysStats)
-            Spacer(modifier = Modifier.height(22.dp))
+            Spacer(modifier = Modifier.height(25.dp))
             RecentProjects(weeklyStatsFlow?.todaysStats)
-            Spacer(modifier = Modifier.height(12.dp))
+            Spacer(modifier = Modifier.height(10.dp))
             WeeklyReport(weeklyStatsFlow?.dailyStats)
+            Spacer(modifier = Modifier.height(25.dp))
             OtherDailyStats()
+            Spacer(modifier = Modifier.height(25.dp))
         }
     }
 
@@ -108,18 +117,52 @@ private fun UserDetailsSection(userDetails: UserDetails?) {
 }
 
 @Composable
-private fun TimeSpentSection(dailyStats: DailyStats?) {
-    TimeSpentCard(
-        Gradients.primary,
-        25,
-        R.drawable.ic_time,
-        "Total Time Spent Today",
-        dailyStats?.timeSpent ?: Time(0, 0, 0f)
-    )
-}
+private fun TimeSpentSection(dailyStats: DailyStats?) = TimeSpentCard(
+    Gradients.primary,
+    25,
+    R.drawable.ic_time,
+    "Total Time Spent Today",
+    dailyStats?.timeSpent ?: Time(0, 0, 0f)
+)
 
 @Composable
 private fun OtherDailyStats() {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Other Daily Stats", fontSize = 28.sp, fontWeight = FontWeight.SemiBold)
+            Text(text = "Details", color = Colors.AccentText, fontSize = 14.sp)
+        }
+        Spacer(modifier = Modifier.height(15.dp))
+        OtherStatsCard(
+            gradient = Gradients.greenCyan,
+            roundedCornerPercent = 25,
+            iconId = R.drawable.ic_code_file,
+            mainText = "Most Language Used",
+            language = "Kotlin"
+        )
+        Spacer(modifier = Modifier.height(15.dp))
+        OtherStatsCard(
+            gradient = Gradients.purpleCyanLight,
+            roundedCornerPercent = 25,
+            iconId = R.drawable.ic_laptop,
+            mainText = "Most OS Used",
+            language = "Linux"
+        )
+        Spacer(modifier = Modifier.height(15.dp))
+        OtherStatsCard(
+            gradient = Gradients.purpleCyanDark,
+            roundedCornerPercent = 25,
+            iconId = R.drawable.ic_code,
+            mainText = "Most Editor Used",
+            language = "Android Studio"
+        )
+    }
 }
 
 @Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
