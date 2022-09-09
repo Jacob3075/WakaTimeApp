47143fb0e927b5f267ecd7a84e475810621487d8
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-09-09 14:03:31 +0530

    WTA #8: Removed DtoMapper and converted classes to extension functions.

--- home/src/main/java/com/jacob/wakatimeapp/home/data/mappers/GetWeeklyStatsResMapper.kt
+++ home/src/main/java/com/jacob/wakatimeapp/home/data/mappers/GetWeeklyStatsResMapper.kt
@@ -1,6 +1,5 @@
 package com.jacob.wakatimeapp.home.data.mappers
 
-import com.jacob.wakatimeapp.core.data.mappers.DtoMapper
 import com.jacob.wakatimeapp.core.models.*
 import com.jacob.wakatimeapp.core.models.Project
 import com.jacob.wakatimeapp.home.data.dtos.GetLast7DaysStatsResDTO
@@ -9,46 +8,40 @@ import com.jacob.wakatimeapp.home.data.dtos.GetLast7DaysStatsResDTO.Data.*
 import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.util.*
-import javax.inject.Inject
 
-class GetWeeklyStatsResMapper @Inject constructor() :
-    DtoMapper<GetLast7DaysStatsResDTO, WeeklyStats> {
-    override fun fromDtoToModel(dto: GetLast7DaysStatsResDTO) = WeeklyStats(
-        totalTime = Time.createFrom(dto.cumulativeTotal.digital, dto.cumulativeTotal.decimal),
-        dailyStats = getDailyStatsFromDto(dto.data),
-        range = StatsRange(
-            startDate = parseDate(dto.start),
-            endDate = parseDate(dto.end)
-        )
+fun GetLast7DaysStatsResDTO.toModel() = WeeklyStats(
+    totalTime = Time.createFrom(cumulativeTotal.digital, cumulativeTotal.decimal),
+    dailyStats = getDailyStatsFromDto(data),
+    range = StatsRange(
+        startDate = parseDate(start),
+        endDate = parseDate(end)
     )
+)
 
-    private fun getDailyStatsFromDto(data: List<Data>) = data.map {
-        DailyStats(
-            timeSpent = Time.createFrom(it.grandTotal.digital, it.grandTotal.decimal),
-            mostUsedEditor = it.editors.maxByOrNull(Editor::percent)?.name ?: "NA",
-            mostUsedLanguage = it.languages.maxByOrNull(Language::percent)?.name ?: "NA",
-            mostUsedOs = it.operatingSystems.maxByOrNull(OperatingSystem::percent)?.name ?: "NA",
-            date = LocalDate.parse(it.range.date),
-            projectsWorkedOn = it.projects.filterNot { project -> project.name == "Unknown Project" }
-                .map { project ->
-                    Project(
-                        Time(
-                            project.hours,
-                            project.minutes,
-                            project.decimal.toFloat()
-                        ),
-                        project.name,
-                        project.percent
-                    )
-                }
-        )
-    }
+private fun getDailyStatsFromDto(data: List<Data>) = data.map {
+    DailyStats(
+        timeSpent = Time.createFrom(it.grandTotal.digital, it.grandTotal.decimal),
+        mostUsedEditor = it.editors.maxByOrNull(Editor::percent)?.name ?: "NA",
+        mostUsedLanguage = it.languages.maxByOrNull(Language::percent)?.name ?: "NA",
+        mostUsedOs = it.operatingSystems.maxByOrNull(OperatingSystem::percent)?.name ?: "NA",
+        date = LocalDate.parse(it.range.date),
+        projectsWorkedOn = it.projects.filterNot { project -> project.name == "Unknown Project" }
+            .map { project ->
+                Project(
+                    Time(
+                        project.hours,
+                        project.minutes,
+                        project.decimal.toFloat()
+                    ),
+                    project.name,
+                    project.percent
+                )
+            }
+    )
+}
 
-    private fun parseDate(dateTimeString: String): Date {
-        val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US)
-        sdf.timeZone = TimeZone.getTimeZone("GMT")
-        return sdf.parse(dateTimeString)!!
-    }
-
-    override fun fromModelToDto(model: WeeklyStats): GetLast7DaysStatsResDTO = TODO()
+private fun parseDate(dateTimeString: String): Date {
+    val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US)
+    sdf.timeZone = TimeZone.getTimeZone("GMT")
+    return sdf.parse(dateTimeString)!!
 }
--- login/src/main/java/com/jacob/wakatimeapp/login/usecases/UpdateUserDetailsUC.kt
+++ login/src/main/java/com/jacob/wakatimeapp/login/usecases/UpdateUserDetailsUC.kt
@@ -2,18 +2,17 @@ package com.jacob.wakatimeapp.login.usecases
 
 import com.jacob.wakatimeapp.core.data.OfflineDataStore
 import com.jacob.wakatimeapp.login.data.LoginPageAPI
-import com.jacob.wakatimeapp.login.data.mappers.UserDetailsMapper
+import com.jacob.wakatimeapp.login.data.mappers.toModel
 import javax.inject.Inject
 
 class UpdateUserDetailsUC @Inject constructor(
     private val loginPageAPI: LoginPageAPI,
     private val offlineDataStore: OfflineDataStore,
-    private val userDetailsMapper: UserDetailsMapper,
 ) {
     suspend operator fun invoke(token: String) {
         val userDetailsResponse = loginPageAPI.getUserDetails("Bearer $token")
         if (userDetailsResponse.isSuccessful) {
-            val userDetails = userDetailsMapper.fromDtoToModel(userDetailsResponse.body()!!)
+            val userDetails = userDetailsResponse.body()!!.toModel()
             offlineDataStore.updateUserDetails(userDetails)
         }
     }
