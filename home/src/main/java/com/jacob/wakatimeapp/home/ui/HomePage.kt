7d60f74fb00fcd8b8f41ea20a94226ec8e1bd2ef
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-09-11 14:39:31 +0530

    WTA #2: Fixed HomePage error flow collection inside LaunchedEffect.

--- home/src/main/java/com/jacob/wakatimeapp/home/ui/HomePage.kt
+++ home/src/main/java/com/jacob/wakatimeapp/home/ui/HomePage.kt
@@ -6,21 +6,16 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Scaffold
 import androidx.compose.material.SnackbarDuration.Long
 import androidx.compose.material.rememberScaffoldState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.jacob.wakatimeapp.core.common.observeInLifecycle
 import com.jacob.wakatimeapp.core.ui.R.drawable
 import com.jacob.wakatimeapp.core.ui.TimeSpentCard
 import com.jacob.wakatimeapp.core.ui.TimeSpentCardParameters
 import com.jacob.wakatimeapp.core.ui.theme.Gradients
 import com.jacob.wakatimeapp.home.ui.components.*
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
@@ -34,17 +29,18 @@ fun HomePageContent(
     val snackBarCoroutineScope = rememberCoroutineScope()
     val viewState by viewModel.homePageState.collectAsState()
 
-    viewModel.errors
-        .onEach {
-            snackBarCoroutineScope.launch {
-                scaffoldState.snackbarHostState.showSnackbar(
-                    message = it.errorMessage,
-                    actionLabel = "Action",
-                    duration = Long
-                )
+    LaunchedEffect(Unit) {
+        viewModel.errors
+            .collectLatest {
+                snackBarCoroutineScope.launch {
+                    scaffoldState.snackbarHostState.showSnackbar(
+                        message = it.errorMessage,
+                        actionLabel = "Action",
+                        duration = Long
+                    )
+                }
             }
-        }
-        .observeInLifecycle(LocalLifecycleOwner.current)
+    }
 
     Scaffold(
         scaffoldState = scaffoldState,
