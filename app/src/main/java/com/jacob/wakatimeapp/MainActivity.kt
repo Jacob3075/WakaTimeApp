728a8d229a397192e5bcacf225efe751cc3eb180
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-09-17 12:48:45 +0530

    Forced app to always be in portrait mode.

--- app/src/main/java/com/jacob/wakatimeapp/MainActivity.kt
+++ app/src/main/java/com/jacob/wakatimeapp/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.jacob.wakatimeapp
 
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.jacob.wakatimeapp.core.ui.theme.WakaTimeAppTheme
 import com.jacob.wakatimeapp.navigation.ApplicationNavigator
@@ -28,6 +35,7 @@ class MainActivity : AppCompatActivity() {
                     scaffoldState = scaffoldState,
                     modifier = Modifier.fillMaxSize()
                 ) {
+                    LockScreenOrientation()
                     DestinationsNavHost(
                         navGraph = NavGraphs.root,
                         dependenciesContainerBuilder = {
@@ -39,4 +47,23 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+}
+
+@Composable
+private fun LockScreenOrientation() {
+    val context = LocalContext.current
+    DisposableEffect(Unit) {
+        val activity = context.findActivity() ?: return@DisposableEffect onDispose {}
+        val originalOrientation = activity.requestedOrientation
+        activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        onDispose {
+            activity.requestedOrientation = originalOrientation
+        }
+    }
+}
+
+private fun Context.findActivity(): Activity? = when (this) {
+    is Activity -> this
+    is ContextWrapper -> baseContext.findActivity()
+    else -> null
 }
