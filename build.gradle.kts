0b0a0d794075d9933e2994390a95a60144932891
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-09-23 13:38:23 +0530

    WTA #12: Moved detekt configuration to subprojects inside root build file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,48 @@ buildscript {
 }
 
 plugins {
-    id("com.google.devtools.ksp") version ("1.7.10-1.0.6") apply false
+    id("com.google.devtools.ksp") version "1.7.10-1.0.6" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.22.0-RC1"
 }
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
+}
+
+// This is a workaround for https://github.com/detekt/detekt/issues/3490
+subprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    dependencies {
+        detektPlugins("ru.kode:detekt-rules-compose:1.2.1")
+    }
+
+    detekt {
+        // Builds the AST in parallel. Rules are always executed in parallel.
+        // Can lead to speedups in larger projects. `false` by default.
+        parallel = true
+
+        // Define the detekt configuration(s) you want to use.
+        // Defaults to the default detekt configuration.
+        config = files("${project.rootDir}/tools/detekt-config.yml")
+
+        // Applies the config files on top of detekt's default config file. `false` by default.
+        buildUponDefaultConfig = true
+
+        // Turns on all the rules. `false` by default.
+        allRules = false
+    }
+}
+
+tasks.detekt.configure {
+    reports {
+        // Enable/Disable HTML report (default: true)
+        html.required.set(true)
+        html.outputLocation.set(file("tools/reports/detekt.html"))
+
+        // Enable/Disable MD report (default: false)
+        md.required.set(true)
+        md.outputLocation.set(file("tools/reports/detekt.md"))
+    }
+    jvmTarget = "11"
 }
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -25,5 +25,4 @@ buildscript {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-serialization:1.7.10")
     implementation("com.google.devtools.ksp:symbol-processing-api:1.7.10-1.0.6")
-    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.22.0-RC1")
 }
