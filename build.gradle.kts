3f5b306a2ab53b345fe3334e0e68969c4c344cc2
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-09-23 21:11:06 +0530

    WTA #12: Moved detekt from root build file back to gradle plugin file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,34 +13,8 @@ buildscript {
 
 plugins {
     id("com.google.devtools.ksp") version "1.7.10-1.0.6" apply false
-    id("io.gitlab.arturbosch.detekt") version "1.22.0-RC1"
 }
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
-}
-
-// This is a workaround for https://github.com/detekt/detekt/issues/3490
-subprojects {
-    apply(plugin = "io.gitlab.arturbosch.detekt")
-
-    dependencies {
-        detektPlugins("ru.kode:detekt-rules-compose:1.2.1")
-    }
-
-    detekt {
-        parallel = true
-        config = files("${rootProject.rootDir}/tools/detekt-config.yml")
-        buildUponDefaultConfig = true
-    }
-
-    tasks.detekt.configure {
-        reports {
-            md.required.set(true)
-            md.outputLocation.set(
-                file("${rootProject.rootDir}/tools/reports/${this@subprojects.name}-detekt.md")
-            )
-        }
-        jvmTarget = "11"
-    }
 }
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -23,6 +23,10 @@ buildscript {
 }
 
 dependencies {
+    implementation("com.android.tools.build:gradle:7.2.2")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+
     implementation("org.jetbrains.kotlin:kotlin-serialization:1.7.10")
     implementation("com.google.devtools.ksp:symbol-processing-api:1.7.10-1.0.6")
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.22.0-RC1")
 }
