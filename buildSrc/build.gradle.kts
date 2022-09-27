bdd2ab6c57feab1a7f65895c46c540ee384c2fbd
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-09-27 11:38:21 +0530

    WTA #12: Fixed build failing and updated dep versions.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     `kotlin-dsl`
     id("groovy-gradle-plugin")
@@ -7,6 +9,11 @@ java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
 }
+
+tasks.withType<KotlinCompile>()
+    .configureEach {
+        kotlinOptions.jvmTarget = "11"
+    }
 
 repositories {
     mavenCentral()
@@ -29,4 +36,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-serialization:1.7.10")
     implementation("com.google.devtools.ksp:symbol-processing-api:1.7.10-1.0.6")
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.22.0-RC1")
+
+    // https://github.com/google/dagger/issues/3068#issuecomment-999118496
+    implementation("com.squareup:javapoet:1.13.0")
 }
