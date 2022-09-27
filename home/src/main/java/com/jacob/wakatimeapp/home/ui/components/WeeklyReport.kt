61787919ea0ae9b5c3c02a117506cd93e4011b68
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-09-27 13:19:51 +0530

    WTA #12: Extracted WeeklyReport chart into smaller functions.

--- home/src/main/java/com/jacob/wakatimeapp/home/ui/components/WeeklyReport.kt
+++ home/src/main/java/com/jacob/wakatimeapp/home/ui/components/WeeklyReport.kt
@@ -28,11 +28,10 @@ import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
 import com.github.mikephil.charting.formatter.ValueFormatter
+import com.jacob.wakatimeapp.core.common.getDisplayNameForDay
 import com.jacob.wakatimeapp.core.models.DailyStats
 import com.jacob.wakatimeapp.core.ui.theme.Colors
 import com.jacob.wakatimeapp.core.ui.theme.WakaTimeAppTheme
-import java.time.format.TextStyle.SHORT
-import java.util.*
 
 @Composable
 fun WeeklyReport(
@@ -65,15 +64,51 @@ private fun WeeklyReportChart(dailyStats: List<DailyStats>) {
         }
     }
 
-    val labels by remember {
+    val labels by getLabels(pairList)
+    val barData by getBarData(pairList)
+
+    Box(
+        modifier = Modifier
+            .padding(horizontal = 10.dp)
+            .shadow(elevation = 8.dp, shape = cardShape)
+            .aspectRatio(1.4f)
+            .background(Colors.CardBGPrimary, shape = cardShape)
+    ) {
+        AndroidView(
+            modifier = Modifier.padding(8.dp),
+            factory = {
+                RoundedBarChart(it).apply {
+                    layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+
+                    configureChartProperties()
+                    configureAxis(labels)
+
+                    data = barData
+                    invalidate()
+                }
+            },
+            update = {
+                it.data = barData
+                it.xAxis.valueFormatter = XAxisDayFormatter(labels)
+                it.invalidate()
+            }
+        )
+    }
+}
+
+@Composable
+private fun getLabels(pairList: List<Pair<Int, DailyStats>>) =
+    remember {
         derivedStateOf {
             pairList.associate { (index, value) ->
-                index to value.date.dayOfWeek.getDisplayName(SHORT, Locale.getDefault())
+                index to value.date.getDisplayNameForDay()
             }
         }
     }
 
-    val barData by remember {
+@Composable
+private fun getBarData(pairList: List<Pair<Int, DailyStats>>) =
+    remember {
         derivedStateOf {
             val entries = pairList.map { (index, value) ->
                 BarEntry(
@@ -90,59 +125,36 @@ private fun WeeklyReportChart(dailyStats: List<DailyStats>) {
         }
     }
 
-    Box(
-        modifier = Modifier
-            .padding(horizontal = 10.dp)
-            .shadow(elevation = 8.dp, shape = cardShape)
-            .aspectRatio(1.4f)
-            .background(Colors.CardBGPrimary, shape = cardShape)
-    ) {
-        AndroidView(
-            modifier = Modifier.padding(8.dp),
-            factory = {
-                RoundedBarChart(it).apply {
-                    setRadius(10)
-
-                    setScaleEnabled(false)
-                    setPinchZoom(false)
-                    isDoubleTapToZoomEnabled = false
-                    description.isEnabled = false
-
-                    xAxis.apply {
-                        setDrawGridLines(false)
-                        textColor = Color.WHITE
-                        position = BOTTOM
-                        valueFormatter = XAxisDayFormatter(labels)
-                    }
-                    axisLeft.apply {
-                        setDrawGridLines(true)
-                        isEnabled = true
-                        spaceBottom = 0f
-                        labelCount = 3
-                        textColor = Color.WHITE
-                        textSize = 8f
-                        valueFormatter = YAxisHourFormatter()
-                        axisMinimum = 0.1f // Can't be zero because of RoundedBarChart
-                    }
-                    axisRight.setDrawGridLines(false)
-                    axisRight.isEnabled = false
-
-                    legend.isEnabled = false
-
-                    layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
-                    data = barData
-                    invalidate()
-
-                    animateY(3000, Easing.EaseInOutBack)
-                }
-            },
-            update = {
-                it.data = barData
-                it.xAxis.valueFormatter = XAxisDayFormatter(labels)
-                it.invalidate()
-            }
-        )
+private fun RoundedBarChart.configureAxis(labels: Map<Int, String>) {
+    xAxis.apply {
+        setDrawGridLines(false)
+        textColor = Color.WHITE
+        position = BOTTOM
+        valueFormatter = XAxisDayFormatter(labels)
     }
+    axisLeft.apply {
+        setDrawGridLines(true)
+        isEnabled = true
+        spaceBottom = 0f
+        labelCount = 3
+        textColor = Color.WHITE
+        textSize = 8f
+        valueFormatter = YAxisHourFormatter()
+        axisMinimum = 0.1f // Can't be zero because of RoundedBarChart
+    }
+    axisRight.setDrawGridLines(false)
+    axisRight.isEnabled = false
+}
+
+private fun RoundedBarChart.configureChartProperties() {
+    setRadius(10)
+
+    setScaleEnabled(false)
+    setPinchZoom(false)
+    isDoubleTapToZoomEnabled = false
+    description.isEnabled = false
+    legend.isEnabled = false
+    animateY(3000, Easing.EaseInOutBack)
 }
 
 @Preview(showBackground = true, showSystemUi = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
