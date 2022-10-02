de416b14e74d6e69880822adbc5ad68b41be82e4
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-10-02 15:04:14 +0530

    WTA #3: Updated app theme with font values.

--- core/ui/src/main/java/com/jacob/wakatimeapp/core/ui/theme/Type.kt
+++ core/ui/src/main/java/com/jacob/wakatimeapp/core/ui/theme/Type.kt
@@ -4,9 +4,10 @@ import androidx.compose.material.Typography
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.jacob.wakatimeapp.core.ui.R
 
-val poppinsFontFamily = FontFamily(
+private val poppinsFontFamily = FontFamily(
     Font(R.font.poppins_extralight, FontWeight.ExtraLight),
     Font(R.font.poppins_light, FontWeight.Light),
     Font(R.font.poppins_regular, FontWeight.Normal),
@@ -17,7 +18,7 @@ val poppinsFontFamily = FontFamily(
     Font(R.font.poppins_black, FontWeight.Black),
 )
 
-val interFontFamily = FontFamily(
+private val interFontFamily = FontFamily(
     Font(R.font.inter_extralight, FontWeight.ExtraLight),
     Font(R.font.inter_light, FontWeight.Light),
     Font(R.font.inter_regular, FontWeight.Normal),
@@ -28,7 +29,7 @@ val interFontFamily = FontFamily(
     Font(R.font.inter_black, FontWeight.Black),
 )
 
-val latoFontFamily = FontFamily(
+private val latoFontFamily = FontFamily(
     Font(R.font.lato_thin, FontWeight.Thin),
     Font(R.font.lato_light, FontWeight.Light),
     Font(R.font.lato_regular, FontWeight.Normal),
@@ -36,6 +37,49 @@ val latoFontFamily = FontFamily(
     Font(R.font.lato_black, FontWeight.Black),
 )
 
+private val defaultTypography = Typography()
+
 val Typography = Typography(
     defaultFontFamily = latoFontFamily,
+    subtitle1 = defaultTypography.subtitle1.copy(
+        fontSize = 18.sp,
+        fontWeight = FontWeight.SemiBold,
+    ),
+    h4 = defaultTypography.h4.copy(
+        fontWeight = FontWeight.SemiBold,
+    ),
+    h5 = defaultTypography.h5.copy(
+        fontSize = 28.sp,
+        fontWeight = FontWeight.SemiBold,
+    ),
+    h6 = defaultTypography.h6.copy(
+        fontSize = 22.sp,
+        fontWeight = FontWeight.Normal,
+    ),
 )
+
+val Typography.sectionTitle
+    get() = h5.copy(
+        fontSize = 28.sp,
+        fontWeight = FontWeight.SemiBold,
+    )
+
+val Typography.sectionSubtitle
+    get() = body2
+
+val Typography.cardHeader
+    get() = h6.copy(
+        fontSize = 22.sp,
+        fontWeight = FontWeight.Normal,
+    )
+
+val Typography.cardSubtitle
+    get() = body2.copy(
+        fontWeight = FontWeight.Light,
+    )
+
+val Typography.cardContent
+    get() = subtitle1.copy(
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Normal,
+    )
