ed27df3935e9307c406639fc3cf1a38269f8b69c
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-10-05 14:26:46 +0530

    WTA #22: Updated home page viewModel and GetLast7DaysStatsUC to use arrow.

--- home/src/main/java/com/jacob/wakatimeapp/home/ui/HomePageViewModel.kt
+++ home/src/main/java/com/jacob/wakatimeapp/home/ui/HomePageViewModel.kt
@@ -3,23 +3,25 @@ package com.jacob.wakatimeapp.home.ui
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import arrow.core.Either.Left
+import arrow.core.Either.Right
 import com.jacob.wakatimeapp.core.common.auth.AuthDataStore
-import com.jacob.wakatimeapp.core.models.Result
+import com.jacob.wakatimeapp.home.ui.HomePageViewState.Loaded
 import com.jacob.wakatimeapp.home.usecases.GetLast7DaysStatsUC
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
-import timber.log.Timber
 
 @HiltViewModel
 class HomePageViewModel @Inject constructor(
     application: Application,
     authDataStore: AuthDataStore,
-    private val ioDispatcher: CoroutineContext,
+    ioDispatcher: CoroutineContext,
     private val getLast7DaysStatsUC: GetLast7DaysStatsUC,
 ) : AndroidViewModel(application) {
 
@@ -29,19 +31,20 @@ class HomePageViewModel @Inject constructor(
 
     init {
         viewModelScope.launch(ioDispatcher) {
-            val result = getLast7DaysStatsUC()
+            val deferredResult = async { getLast7DaysStatsUC() }
 
             authDataStore.getUserDetails()
                 .distinctUntilChanged()
                 .collect { userDetails ->
-                    when (result) {
-                        is Result.Success -> _homePageState.value = HomePageViewState.Loaded(
+                    when (val result = deferredResult.await()) {
+                        is Right -> _homePageState.value = Loaded(
                             result.value,
                             userDetails
                         )
-                        is Result.Failure -> _homePageState.value = result.getErrorMessage()
-                            .let(HomePageViewState::Error)
-                        is Result.Empty -> Timber.e("EMPTY")
+
+                        is Left ->
+                            _homePageState.value =
+                                result.value.message.let(HomePageViewState::Error)
                     }
                 }
         }
--- home/src/main/java/com/jacob/wakatimeapp/home/usecases/GetLast7DaysStatsUC.kt
+++ home/src/main/java/com/jacob/wakatimeapp/home/usecases/GetLast7DaysStatsUC.kt
@@ -1,9 +1,10 @@
 package com.jacob.wakatimeapp.home.usecases
 
-import com.jacob.wakatimeapp.core.models.ErrorTypes.NetworkError
-import com.jacob.wakatimeapp.core.models.Result
-import com.jacob.wakatimeapp.core.models.Result.Failure
-import com.jacob.wakatimeapp.core.models.Result.Success
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
+import com.jacob.wakatimeapp.core.models.Error
+import com.jacob.wakatimeapp.core.models.Error.NetworkErrors
 import com.jacob.wakatimeapp.core.models.WeeklyStats
 import com.jacob.wakatimeapp.home.data.HomePageNetworkData
 import com.jacob.wakatimeapp.home.data.dtos.GetLast7DaysStatsResDTO
@@ -14,22 +15,24 @@ import timber.log.Timber
 class GetLast7DaysStatsUC @Inject constructor(
     private val homePageNetworkData: HomePageNetworkData,
 ) {
-    @Suppress("TooGenericExceptionCaught")
-    suspend operator fun invoke(): Result<WeeklyStats> {
-        try {
-            val statsForTodayResponse = homePageNetworkData.getLast7DaysStats()
-            if (!statsForTodayResponse.isSuccessful) return Failure(
-                NetworkError(Exception(statsForTodayResponse.message()))
+    suspend operator fun invoke(): Either<Error, WeeklyStats> = Either.catchAndFlatten {
+        val statsForTodayResponse = homePageNetworkData.getLast7DaysStats()
+
+        if (!statsForTodayResponse.isSuccessful) {
+            return@catchAndFlatten NetworkErrors.create(
+                statsForTodayResponse.message(),
+                statsForTodayResponse.code()
             )
-
-            val weeklyStats =
-                statsForTodayResponse.body()!!
-                    .run(GetLast7DaysStatsResDTO::toModel)
-
-            return Success(weeklyStats)
-        } catch (exception: Exception) {
-            Timber.e(exception.message)
-            return Failure(NetworkError(exception))
+                .left()
         }
+
+        val weeklyStats = statsForTodayResponse.body()!!
+            .run(GetLast7DaysStatsResDTO::toModel)
+
+        weeklyStats.right()
     }
+        .mapLeft {
+            Timber.e(it.message)
+            NetworkErrors.create(it.message!!)
+        }
 }
