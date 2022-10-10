8d80fc3e539001a332e4cf6dffcc3fe52d5fac07
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-10-10 21:19:48 +0530

    WTA #31: Added deps for room in home module.

--- home/build.gradle.kts
+++ home/build.gradle.kts
@@ -8,6 +8,12 @@ dependencies {
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 
     implementation("androidx.datastore:datastore-preferences:1.0.0")
+
+    implementation("androidx.room:room-runtime:2.4.3")
+    implementation("androidx.room:room-ktx:2.4.3")
+
+    annotationProcessor("androidx.room:room-compiler:2.4.3")
+    ksp("androidx.room:room-compiler:2.4.3")
 }
 android {
     namespace = "com.jacob.wakatimeapp.home"
