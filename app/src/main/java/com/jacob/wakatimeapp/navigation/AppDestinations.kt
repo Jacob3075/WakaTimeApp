0faaba8ff2b529866b21e0b1145c7222e5c66ef5
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-10-16 23:00:42 +0530

    WTA #20: Updated HomePageScreen to have previews for different states.

--- app/src/main/java/com/jacob/wakatimeapp/navigation/AppDestinations.kt
+++ app/src/main/java/com/jacob/wakatimeapp/navigation/AppDestinations.kt
@@ -2,8 +2,8 @@ package com.jacob.wakatimeapp.navigation
 
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
-import com.jacob.wakatimeapp.home.ui.HomePageContent
 import com.jacob.wakatimeapp.home.ui.HomePageNavigator
+import com.jacob.wakatimeapp.home.ui.HomePageScreen
 import com.jacob.wakatimeapp.login.ui.LoginPageNavigator
 import com.jacob.wakatimeapp.login.ui.LoginPageScreen
 import com.ramcosta.composedestinations.annotation.Destination
@@ -20,7 +20,7 @@ fun LoginPage(loginPageNavigator: LoginPageNavigator, scaffoldState: SnackbarHos
 fun HomePage(
     homePageNavigator: HomePageNavigator,
     scaffoldState: SnackbarHostState,
-) = HomePageContent(
+) = HomePageScreen(
     navigator = homePageNavigator,
     snackbarHostState = scaffoldState
 )
