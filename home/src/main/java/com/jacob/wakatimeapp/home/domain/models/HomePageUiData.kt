1b6559cc7ba5aa536c8b0abf33ba440a8570cb35
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-10-17 13:56:23 +0530

    WTA #39: Updated how home page loaded state data is represented.

--- home/src/main/java/com/jacob/wakatimeapp/home/domain/models/HomePageUiData.kt
+++ home/src/main/java/com/jacob/wakatimeapp/home/domain/models/HomePageUiData.kt
@@ -5,27 +5,49 @@ import com.jacob.wakatimeapp.core.models.Time
 import com.jacob.wakatimeapp.core.models.UserDetails
 import com.jacob.wakatimeapp.core.models.WeeklyStats
 import kotlinx.datetime.LocalDate
+import kotlinx.datetime.daysUntil
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class HomePageUiData(
+data class Last7DaysStats(
     val timeSpentToday: Time,
     val projectsWorkedOn: List<Project>,
     val weeklyTimeSpent: Map<LocalDate, Time>,
     val mostUsedLanguage: String,
     val mostUsedEditor: String,
     val mostUsedOs: String,
-    val photoUrl: String,
-    val fullName: String,
 )
 
-fun WeeklyStats.toLoadedStateData(userDetails: UserDetails) = HomePageUiData(
+@Serializable
+data class HomePageUserDetails(
+    val fullName: String,
+    val photoUrl: String,
+)
+
+@Serializable
+data class Streaks(
+    val currentStreak: StreakRange,
+    val longestStreak: StreakRange,
+)
+
+@Serializable
+data class StreakRange(
+    val start: LocalDate,
+    val end: LocalDate,
+) {
+    val days = start.daysUntil(end)
+}
+
+fun WeeklyStats.toLoadedStateData() = Last7DaysStats(
     timeSpentToday = todaysStats.timeSpent,
     projectsWorkedOn = todaysStats.projectsWorkedOn,
     weeklyTimeSpent = dailyStats.associate { it.date to it.timeSpent },
     mostUsedLanguage = todaysStats.mostUsedLanguage,
     mostUsedEditor = todaysStats.mostUsedEditor,
     mostUsedOs = todaysStats.mostUsedOs,
-    photoUrl = userDetails.photoUrl,
-    fullName = userDetails.fullName,
+)
+
+fun UserDetails.toHomePageUserDetails() = HomePageUserDetails(
+    fullName = fullName,
+    photoUrl = photoUrl
 )
