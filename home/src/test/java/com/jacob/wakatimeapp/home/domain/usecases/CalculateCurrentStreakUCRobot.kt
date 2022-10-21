825a938402af2844fe2d09aedcfe34495ca17117
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-10-21 12:13:13 +0530

    WTA #44: Fixed GetLast7DaysStatsUC tests.

--- home/src/test/java/com/jacob/wakatimeapp/home/domain/usecases/CalculateCurrentStreakUCRobot.kt
+++ home/src/test/java/com/jacob/wakatimeapp/home/domain/usecases/CalculateCurrentStreakUCRobot.kt
@@ -7,9 +7,6 @@ import com.jacob.wakatimeapp.core.models.Time
 import com.jacob.wakatimeapp.home.data.local.HomePageCache
 import com.jacob.wakatimeapp.home.domain.models.Last7DaysStats
 import com.jacob.wakatimeapp.home.domain.models.StreakRange
-import io.kotest.assertions.arrow.core.shouldBeLeft
-import io.kotest.assertions.arrow.core.shouldBeRight
-import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.shouldBe
 import io.mockk.clearMocks
 import io.mockk.coEvery
@@ -17,7 +14,6 @@ import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.datetime.LocalDate
 
@@ -25,13 +21,13 @@ import kotlinx.datetime.LocalDate
 internal class CalculateCurrentStreakUCRobot {
     private lateinit var useCase: CalculateCurrentStreakUC
 
-    private val results: MutableList<Either<Error, StreakRange>> = mutableListOf()
+    private var result: Error? = null
 
     private val mockCache: HomePageCache = mockk(relaxUnitFun = true)
 
     fun buildUseCase() = apply {
         clearMocks(mockCache)
-        results.clear()
+        result = null
 
         useCase = CalculateCurrentStreakUC(
             dispatcher = UnconfinedTestDispatcher(),
@@ -40,31 +36,11 @@ internal class CalculateCurrentStreakUCRobot {
     }
 
     suspend fun callUseCase() = apply {
-        useCase().toList(results)
+        result = useCase()
     }
 
-    fun resultSizeShouldBe(size: Int = 1) = apply {
-        results.size shouldBe size
-    }
-
-    fun resultsShouldContain(expected: Either<Error, StreakRange>) = apply {
-        results shouldContain expected
-    }
-
-    fun resultsShouldContain(expected: StreakRange) = apply {
-        results.first() shouldBeRight expected
-    }
-
-    fun resultShouldBeRight() = apply {
-        results.first().shouldBeRight()
-    }
-
-    fun resultsShouldContain(expected: Error) = apply {
-        results.first() shouldBeLeft expected
-    }
-
-    fun resultShouldBeLeft() = apply {
-        results.first().shouldBeLeft()
+    fun resultsShouldBe(expected: Error?) = apply {
+        result shouldBe expected
     }
 
     fun mockGetCurrentStreak(data: Either<Error, StreakRange>) = apply {
--- home/src/test/java/com/jacob/wakatimeapp/home/domain/usecases/GetLast7DaysStatsUCRobot.kt
+++ home/src/test/java/com/jacob/wakatimeapp/home/domain/usecases/GetLast7DaysStatsUCRobot.kt
@@ -1,7 +1,6 @@
 package com.jacob.wakatimeapp.home.domain.usecases
 
 import arrow.core.Either
-import arrow.core.right
 import com.jacob.wakatimeapp.core.models.DailyStats
 import com.jacob.wakatimeapp.core.models.Error
 import com.jacob.wakatimeapp.core.models.StatsRange
@@ -9,123 +8,53 @@ import com.jacob.wakatimeapp.core.models.Time
 import com.jacob.wakatimeapp.core.models.WeeklyStats
 import com.jacob.wakatimeapp.home.data.local.HomePageCache
 import com.jacob.wakatimeapp.home.data.network.HomePageNetworkData
-import com.jacob.wakatimeapp.home.domain.InstantProvider
-import com.jacob.wakatimeapp.home.domain.models.Last7DaysStats
-import com.jacob.wakatimeapp.home.domain.usecases.GetLast7DaysStatsUC.CacheValidity.DEFAULT
-import io.kotest.matchers.collections.shouldContain
-import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.shouldBe
 import io.mockk.clearMocks
 import io.mockk.coEvery
-import io.mockk.coJustRun
 import io.mockk.coVerify
 import io.mockk.mockk
-import kotlin.time.Duration.Companion.days
-import kotlin.time.Duration.Companion.minutes
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
-import kotlinx.datetime.Clock
-import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDate
-import kotlinx.datetime.TimeZone
 
 @OptIn(ExperimentalCoroutinesApi::class)
 internal class GetLast7DaysStatsUCRobot {
-    private val results: MutableList<Either<Error, Last7DaysStats>> = mutableListOf()
     private lateinit var useCase: GetLast7DaysStatsUC
+
+    private var result: Error? = null
 
     private val networkDataMock: HomePageNetworkData = mockk(relaxUnitFun = true)
     private val cacheMock: HomePageCache = mockk(relaxUnitFun = true)
 
-    fun build(instantProvider: InstantProvider? = null) = apply {
+    fun buildViewModel() = apply {
         clearMocks(networkDataMock, cacheMock)
+        result = null
 
         useCase = GetLast7DaysStatsUC(
             dispatcher = UnconfinedTestDispatcher(),
             homePageNetworkData = networkDataMock,
             homePageCache = cacheMock,
-            instantProvider = instantProvider ?: object : InstantProvider {
-                override val timeZone = TimeZone.UTC
-
-                override fun now() = Clock.System.now()
-            },
         )
     }
 
     suspend fun callUseCase() = apply {
-        useCase(DEFAULT).toList(results)
+        result = useCase()
     }
 
-    fun resultSizeShouldBe(size: Int = 1) = apply {
-        results.size shouldBe size
-    }
-
-    fun resultsShouldContain(expected: Either<Error, Last7DaysStats>) = apply {
-        results shouldContain expected
-    }
-
-    fun resultsShouldContain(expected: List<Either<Error, Last7DaysStats>>) = apply {
-        results shouldContainExactly expected
-    }
-
-    fun mockCacheLastRequestTime(instant: Instant) = apply {
-        coEvery { cacheMock.getLastRequestTime() } returns instant
-    }
-
-    fun mockUpdateCacheData(data: Last7DaysStats) = apply {
-        coJustRun { cacheMock.updateLast7DaysStats(data) }
-    }
-
-    fun mockCachedData(vararg data: Last7DaysStats) = apply {
-        coEvery { cacheMock.getLast7DaysStats() } returns (
-            data.map(Last7DaysStats::right)
-                .asFlow()
-            )
+    fun resultsShouldBe(expected: Error?) = apply {
+        result shouldBe expected
     }
 
     fun mockNetworkData(data: Either<Error, WeeklyStats>) = apply {
         coEvery { networkDataMock.getLast7DaysStats() } returns data
     }
 
-    fun verifyCacheGetCachedDataCalled(count: Int = 1) = apply {
-        coVerify(exactly = count) { cacheMock.getLast7DaysStats() }
-    }
-
-    fun verifyCacheSetCachedDataCalled(data: Last7DaysStats, count: Int = 1) = apply {
-        coVerify(exactly = count) { cacheMock.updateLast7DaysStats(eq(data)) }
-    }
-
-    fun verifyCacheUpdateLastRequestTimeCalled(count: Int = 1) = apply {
-        coVerify(exactly = count) { cacheMock.updateLastRequestTime(any()) }
-    }
-
-    fun verifyGetLast7DaysStatsCalled(count: Int = 1) = apply {
-        coVerify(exactly = count) { networkDataMock.getLast7DaysStats() }
+    fun verifyUpdateLast7DaysStatsCacheCalled(count: Int = 1) = apply {
+        coVerify(exactly = count) { cacheMock.updateLast7DaysStats(any()) }
     }
 
     companion object {
-
-        val previousDay = Clock.System.now()
-            .minus(1.days)
-
-        val validDataInstant = Clock.System.now()
-            .minus(10.minutes)
-
-        val invalidDataInstant = Clock.System.now()
-            .minus(20.minutes)
-
         private val todaysDate = LocalDate(2022, 10, 10)
-
-        val homePageUiData = Last7DaysStats(
-            timeSpentToday = Time.ZERO,
-            projectsWorkedOn = listOf(),
-            weeklyTimeSpent = mapOf(),
-            mostUsedLanguage = "",
-            mostUsedEditor = "",
-            mostUsedOs = "",
-        )
 
         val weeklyStats = WeeklyStats(
             totalTime = Time.ZERO,
