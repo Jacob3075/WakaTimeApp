6c04f20f01705df36b638387ffc299e045274590
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-11-30 12:08:05 +0530

    WTA #47: Updated buildSrc plugin scripts to use version catalog.

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     buildFeatures { compose = true }
-    composeOptions { kotlinCompilerExtensionVersion = "1.3.1" }
+    composeOptions { kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get() }
 
     kotlinOptions { jvmTarget = "11" }
     namespace = "com.jacob.wakatimeapp.core.ui"
@@ -16,25 +16,24 @@ dependencies {
     implementation(project(":core:common"))
     implementation(project(":core:models"))
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.5.1")
-    implementation("com.google.android.material:material:1.7.0")
-    implementation("androidx.compose.material3:material3:1.0.0")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.google.material)
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation(libs.kotlinx.coroutines.android)
 
     // Compose
-    implementation(platform("androidx.compose:compose-bom:2022.10.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.material3:material3")
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui.core)
+    implementation(libs.androidx.compose.material3)
 
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.androidx.compose.ui.testManifest)
 
-    implementation("com.airbnb.android:lottie-compose:5.0.3")
+    implementation(libs.lottie.compose)
 
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    testImplementation(libs.junit4)
+    androidTestImplementation(libs.androidx.test.ext)
+    androidTestImplementation(libs.androidx.test.espresso.core)
 }
