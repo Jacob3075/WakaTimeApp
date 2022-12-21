8460b3dcc23003dde755832650b9701231e72573
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-12-21 13:32:54 +0530

    WTA #71: Added basic setup for details page.

---
+++ details/src/main/java/com/jacob/wakatimeapp/details/ui/DetailsPageScreen.kt
@@ -0,0 +1,38 @@
+package com.jacob.wakatimeapp.details.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun DetailsPageScreen(
+    navigator: DetailsPageNavigator,
+    modifier: Modifier = Modifier,
+) = DetailsPageScreen(
+    navigator = navigator,
+    modifier = modifier,
+    viewModel = hiltViewModel(),
+)
+
+@Composable
+private fun DetailsPageScreen(
+    navigator: DetailsPageNavigator,
+    viewModel: DetailsPageViewModel,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = "Details Page",
+            style = MaterialTheme.typography.headlineLarge,
+        )
+    }
+}
