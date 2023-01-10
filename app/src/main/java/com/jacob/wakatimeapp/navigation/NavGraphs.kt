eacade0671463a2e59f6982a9c24928041cd6a0a
Jacob Bosco <jacob.bosco3075@gmail.com>
2023-01-10 15:47:43 +0530

    WTA #71: Updated compose destinations to have multi module set-up.

---
+++ app/src/main/java/com/jacob/wakatimeapp/navigation/NavGraphs.kt
@@ -0,0 +1,23 @@
+package com.jacob.wakatimeapp.navigation
+
+import com.jacob.wakatimeapp.details.ui.destinations.DetailsPageDestination
+import com.jacob.wakatimeapp.home.ui.destinations.HomePageDestination
+import com.jacob.wakatimeapp.login.ui.destinations.LoginPageDestination
+import com.jacob.wakatimeapp.search.ui.destinations.SearchProjectsDestination
+import com.ramcosta.composedestinations.spec.DestinationSpec
+import com.ramcosta.composedestinations.spec.NavGraphSpec
+
+object NavGraphs {
+    val root = object : NavGraphSpec {
+        override val route = "root"
+
+        override val startRoute = LoginPageDestination
+
+        override val destinationsByRoute = listOf<DestinationSpec<*>>(
+            LoginPageDestination,
+            HomePageDestination,
+            SearchProjectsDestination,
+            DetailsPageDestination,
+        ).associateBy { it.route }
+    }
+}
