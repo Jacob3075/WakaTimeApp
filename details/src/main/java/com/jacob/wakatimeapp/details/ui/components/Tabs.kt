cb970ff85ac2484faa053aa7b86e0d114b02e9f8
Jacob Bosco <jacob.bosco3075@gmail.com>
2023-01-21 12:56:25 +0530

    WTA #71: Improved how tabs are handled.

---
+++ details/src/main/java/com/jacob/wakatimeapp/details/ui/components/Tabs.kt
@@ -0,0 +1,26 @@
+package com.jacob.wakatimeapp.details.ui.components
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+internal fun TimeTab(modifier: Modifier = Modifier) {
+    Text("Time", modifier = modifier.fillMaxSize())
+}
+
+@Composable
+internal fun LanguagesTab(modifier: Modifier = Modifier) {
+    Text("Languages", modifier = modifier.fillMaxSize())
+}
+
+@Composable
+internal fun EditorsTab(modifier: Modifier = Modifier) {
+    Text("Editors", modifier = modifier.fillMaxSize())
+}
+
+@Composable
+internal fun OperatingSystemsTab(modifier: Modifier = Modifier) {
+    Text("Operating Systems", modifier = modifier.fillMaxSize())
+}
