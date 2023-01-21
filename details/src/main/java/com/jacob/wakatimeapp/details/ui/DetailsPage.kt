f5e63e5a7e781f9d37ece946a1a415ca855565e5
Jacob Bosco <jacob.bosco3075@gmail.com>
2023-01-21 12:56:25 +0530

    WTA #71: Improved how tabs are handled.

--- details/src/main/java/com/jacob/wakatimeapp/details/ui/DetailsPage.kt
+++ details/src/main/java/com/jacob/wakatimeapp/details/ui/DetailsPage.kt
@@ -3,17 +3,12 @@ package com.jacob.wakatimeapp.details.ui
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarDuration.Long
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
-import androidx.compose.material3.TabRowDefaults
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -21,8 +16,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
@@ -31,7 +24,12 @@ import com.jacob.wakatimeapp.core.ui.components.WtaAnimation
 import com.jacob.wakatimeapp.core.ui.theme.assets
 import com.jacob.wakatimeapp.core.ui.theme.spacing
 import com.jacob.wakatimeapp.details.ui.components.DetailsPageHeader
-import com.jacob.wakatimeapp.details.ui.modiffiers.pagerTabIndicatorOffset
+import com.jacob.wakatimeapp.details.ui.components.EditorsTab
+import com.jacob.wakatimeapp.details.ui.components.LanguagesTab
+import com.jacob.wakatimeapp.details.ui.components.OperatingSystemsTab
+import com.jacob.wakatimeapp.details.ui.components.TabBar
+import com.jacob.wakatimeapp.details.ui.components.Tabs
+import com.jacob.wakatimeapp.details.ui.components.TimeTab
 import com.ramcosta.composedestinations.annotation.Destination
 import kotlinx.coroutines.launch
 
@@ -71,65 +69,50 @@ private fun DetailsPageScreen(
         }
     }
 
-    DetailsPageContent(viewState = viewState, modifier = modifier)
-}
-
-@Composable
-private fun DetailsPageContent(viewState: DetailsPageViewState, modifier: Modifier = Modifier) =
     Column(
         modifier = modifier.statusBarsPadding(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
-        when (viewState) {
+        when (val state = viewState) {
             is DetailsPageViewState.Loading -> DetailsPageLoading()
-            is DetailsPageViewState.Loaded -> DetailsPageLoaded(viewState)
-            is DetailsPageViewState.Error -> DetailsPageError(viewState)
+            is DetailsPageViewState.Loaded -> DetailsPageLoaded(state)
+            is DetailsPageViewState.Error -> DetailsPageError(state)
         }
     }
+}
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
 private fun DetailsPageLoaded(viewState: DetailsPageViewState.Loaded) {
-    val scope = rememberCoroutineScope()
+    val pagerState = rememberPagerState()
+    val pages = listOf(
+        Tabs.Time,
+        Tabs.Languages,
+        Tabs.Editors,
+        Tabs.OperatingSystems,
+    )
+
     Column {
         Spacer(modifier = Modifier.height(MaterialTheme.spacing.small))
+
         DetailsPageHeader(
             viewState.projectName,
             modifier = Modifier.padding(horizontal = MaterialTheme.spacing.extraSmall),
         )
 
-        val pagerState = rememberPagerState()
-        val pages = listOf("Page 1", "Page 2", "Page 3")
-
-        TabRow(
-            selectedTabIndex = pagerState.currentPage,
-            indicator = { tabPositions ->
-                TabRowDefaults.Indicator(
-                    Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
-                )
-            }
-        ) {
-            pages.forEachIndexed { index, title ->
-                Tab(
-                    text = { Text(title) },
-                    selected = pagerState.currentPage == index,
-                    onClick = { scope.launch { pagerState.animateScrollToPage(index) } }
-                )
-            }
-        }
+        TabBar(pagerState, pages)
 
         HorizontalPager(
             count = pages.size,
             state = pagerState,
         ) { page ->
-            Text(
-                text = "Page $page",
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxSize(),
-                textAlign = TextAlign.Center,
-            )
+            when (pages[page]) {
+                Tabs.Time -> TimeTab()
+                Tabs.Languages -> LanguagesTab()
+                Tabs.Editors -> EditorsTab()
+                Tabs.OperatingSystems -> OperatingSystemsTab()
+            }
         }
     }
 }
