c51118f7b0d14099e76418721d9e43adb5646077
Jacob Bosco <jacob.bosco3075@gmail.com>
2023-01-21 10:17:36 +0530

    WTA #71: Added setup for Project details page.

--- details/src/main/java/com/jacob/wakatimeapp/details/ui/DetailsPageViewState.kt
+++ details/src/main/java/com/jacob/wakatimeapp/details/ui/DetailsPageViewState.kt
@@ -3,7 +3,7 @@ package com.jacob.wakatimeapp.details.ui
 sealed class DetailsPageViewState {
     object Loading : DetailsPageViewState()
 
-    data class Loaded(val detUiDetailsPageViewState: DetailsPageViewState) : DetailsPageViewState()
+    data class Loaded(val projectName: String) : DetailsPageViewState()
 
     data class Error(val error: com.jacob.wakatimeapp.core.models.Error) : DetailsPageViewState()
 }
