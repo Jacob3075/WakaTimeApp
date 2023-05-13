a2bec35d596893597ce40b80138c82d91f871b54
Jacob Bosco <jacob.bosco3075@gmail.com>
2023-05-13 18:24:50 +0530

    Fixed Project list screen.

--- core/common/src/main/java/com/jacob/wakatimeapp/core/common/data/dtos/ProjectDetailsDTO.kt
+++ core/common/src/main/java/com/jacob/wakatimeapp/core/common/data/dtos/ProjectDetailsDTO.kt
@@ -19,8 +19,8 @@ data class ProjectDetailsDTO(
         val repository: RepositoryDTO?,
         val url: String,
         @SerialName("created_at") val createdAt: String,
-        @SerialName("human_readable_last_heartbeat_at") val humanReadableLastHeartbeatAt: String,
-        @SerialName("last_heartbeat_at") val lastHeartbeatAt: String,
+        @SerialName("human_readable_last_heartbeat_at") val humanReadableLastHeartbeatAt: String?,
+        @SerialName("last_heartbeat_at") val lastHeartbeatAt: String?,
     ) {
         @Serializable
         data class RepositoryDTO(
