cd3fd0a42199d94ac623c720bf4bd8e89fc80a7e
Jacob Bosco <jacob.bosco3075@gmail.com>
2023-09-13 21:46:07 +0530

    WTA #79: updated enter animation for progress bar

--- home/src/main/java/com/jacob/wakatimeapp/home/ui/extract/ExtractUserDataPage.kt
+++ home/src/main/java/com/jacob/wakatimeapp/home/ui/extract/ExtractUserDataPage.kt
@@ -1,28 +1,39 @@
 package com.jacob.wakatimeapp.home.ui.extract
 
 import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedContentTransitionScope.SlideDirection
+import androidx.compose.animation.core.EaseInOut
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
 import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.jacob.wakatimeapp.core.ui.WtaComponentPreviews
+import com.jacob.wakatimeapp.core.ui.theme.WakaTimeAppTheme
 import com.jacob.wakatimeapp.core.ui.theme.spacing
 import com.jacob.wakatimeapp.home.ui.HomePageNavigator
-import com.jacob.wakatimeapp.home.ui.extract.components.AnimatedProgressButton
-import com.jacob.wakatimeapp.home.ui.extract.components.CreateAndDownloadExtract
+import com.jacob.wakatimeapp.home.ui.extract.components.AnimatedProgressBar
 import com.ramcosta.composedestinations.annotation.Destination
+
+const val AnimationDuration = 250
 
 @Composable
 @Destination
@@ -53,9 +64,24 @@ private fun ExtractUserDataScreen(
             targetState = viewState,
             label = "",
             transitionSpec = {
-                (slideInVertically { height -> height } + fadeIn()) togetherWith
-                    slideOutVertically { height -> -height } + fadeOut()
+                (
+                    slideIntoContainer(
+                        towards = SlideDirection.Up,
+                        animationSpec = tween(
+                            durationMillis = AnimationDuration,
+                            easing = EaseInOut,
+                        ),
+                    ) + fadeIn(tween(durationMillis = AnimationDuration, easing = EaseInOut))
+                    ) togetherWith
+                    slideOutOfContainer(
+                        towards = SlideDirection.Up,
+                        animationSpec = tween(
+                            durationMillis = AnimationDuration,
+                            easing = EaseInOut,
+                        ),
+                    ) + fadeOut(tween(durationMillis = AnimationDuration, easing = EaseInOut))
             },
+            modifier = Modifier.padding(vertical = MaterialTheme.spacing.large),
         ) {
             when (it) {
                 is ExtractPageViewState.Idle -> {
@@ -64,7 +90,7 @@ private fun ExtractUserDataScreen(
                 }
 
                 is ExtractPageViewState.CreatingExtract -> { // show progress bar for creating extract
-                    AnimatedProgressButton((viewState as ExtractPageViewState.CreatingExtract).progress)
+                    AnimatedProgressBar((viewState as ExtractPageViewState.CreatingExtract).progress)
                 }
 
                 else -> Unit
@@ -79,4 +105,48 @@ private fun TransferringAnimation() { // animation showing transferring or copyi
 
 @Composable
 private fun LoadExtractFromFile() { // open file picker to select extract
+}
+
+@Composable
+private fun CreateAndDownloadExtract(
+    createExtract: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Button(
+        onClick = createExtract,
+        modifier = modifier.fillMaxWidth(),
+    ) {
+        Text(text = "Create Extract")
+    }
+}
+
+@WtaComponentPreviews
+@Composable
+private fun CreateAndDownloadExtractPreview() {
+    WakaTimeAppTheme {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+        ) {
+            CreateAndDownloadExtract(createExtract = {})
+        }
+    }
+}
+
+@WtaComponentPreviews
+@Composable
+private fun CreateAndDownloadExtractPreviewWithValue() {
+    var progressValue: Float? by remember { mutableStateOf(null) }
+
+    WakaTimeAppTheme {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .padding(bottom = 50.dp),
+        ) {
+            CreateAndDownloadExtract(createExtract = { progressValue = 0.5f })
+        }
+    }
 }
